81fb02b21539d22c01828a18f9346965024511f7
Simon Brand <simon.brand@postadigitale.de>
2019-02-24 21:41:20 +0100

    Change iso publisher and application

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -e -u
 
 iso_name=archlinux
 iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <http://www.archlinux.org>"
-iso_application="Arch Linux Live/Rescue CD"
+iso_publisher="Yourself"
+iso_application="Arch Linux Secure boot Live/Rescue CD"
 iso_version=$(date +%Y.%m.%d)
 install_dir=arch
 work_dir=work
